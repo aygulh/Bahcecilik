77230687bd145774ba9782b5c25669a75912bbaf
Hidir Aygul <aygulh@gmail.com>
2017-02-17 10:40:54 +0300

    some missing html files added to git and android:configChanges="orientation|screenSize" added to manifest file to solve returning back to first screen after device rotation

--- app/src/main/java/com/xagnhay/bahcecilik/MainActivity.java
+++ app/src/main/java/com/xagnhay/bahcecilik/MainActivity.java
@@ -1,6 +1,7 @@
 package com.xagnhay.bahcecilik;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
